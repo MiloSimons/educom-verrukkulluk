0e3641d8eb1e0765490512e7406064163e492fea
MiloSimons <milo.simons@gmail.com>
2024-10-11 16:50:07 +0200

    vervaardig gerechtInfo class (#8)

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 
-require_once("lib/database.php");
-require_once("lib/artikel.php");
-require_once("lib/user.php");
-require_once("lib/keukenType.php");
-require_once("lib/ingredient.php");
+require_once("lib/Database.php");
+require_once("lib/Artikel.php");
+require_once("lib/User.php");
+require_once("lib/KeukenType.php");
+require_once("lib/Ingredient.php");
+require_once("lib/GerechtInfo.php");
 
 /// INIT
 $db = new Database();
@@ -12,14 +13,25 @@ $art = new Artikel($db->getConnection());
 $usr = new User($db->getConnection());
 $kt = new KeukenType($db->getConnection());
 $ig = new Ingredient($db->getConnection());
+$gerInf = new GerechtInfo($db->getConnection());
 
 
 /// VERWERK 
 $artikelEight = $art->getArtikel(8);
 $pieter = $usr->getUser(1);
-$keukenType1 = $kt->getKeukenType(1);
+//$keukenType1 = $kt->getKeukenType(1);
 
-$ingr = $ig->getIngredients(3); //3 is gerecht_id
+//$ingr = $ig->getIngredients(3); //3 is gerecht_id
+
+$ger3info = $gerInf->getGerechtInfo(3);
+
+$ger1info = $gerInf->getGerechtInfo(1);
+//var_dump($ger1info);
+$gerInf->addFavorite(1, 1);
+//$gerInf->deleteFavorite(1, 1);
+$ger1info = $gerInf->getGerechtInfo(1);
+var_dump($ger1info);
+
 
 //$art_id = $ig->getArtikel_id($ingr["id"]);
 //$artikel2 = $art->getArtikel($art_id);
@@ -33,4 +45,6 @@ $ingr = $ig->getIngredients(3); //3 is gerecht_id
 //var_dump($ingr);
 //var_dump($art_id);
 //var_dump($art3);
-var_dump($ingr);
+
+//var_dump($ingr);
+//var_dump($ger3info);
---
+++ lib/GerechtInfo.php
@@ -0,0 +1,61 @@
+<?php
+//require_once("lib/database.php");
+require_once("lib/User.php");
+
+class GerechtInfo {
+
+    private $connection;
+    private $db;
+    private $usr;
+
+    public function __construct($connection) {
+        $this->connection = $connection;
+        $this->db = new Database();
+        $this->usr = new User($this->db->getConnection());
+    }     
+
+    public function getGerechtInfo($gerecht_id) {
+        
+        $sql = "select * from gerecht_info where gerecht_id = $gerecht_id";
+        
+        $result = mysqli_query($this->connection, $sql);
+
+        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+            echo"<pre>";
+            if($row["record_type"]== "F" || $row["record_type"]== "O") {
+                $user = $this->selectUser($row["user_id"]);
+                $gerecht_info[]=[$row, $user];
+            } else {
+            $gerecht_info[]=$row;
+            }
+        }
+
+        return $gerecht_info;
+    }
+
+    private function selectUser($user_id) {
+        $user = $this->usr->getUser($user_id);
+        return $user;
+    }
+
+    public function addFavorite($gerecht_id, $user_id) { //of moet je hier een user meegeven ipv een user_id? and what about date?
+        // Only add favorite if it does not exists already for that recipe + user combo        
+        $sql = "select * from gerecht_info where gerecht_id = $gerecht_id and user_id = $user_id and record_type = 'F'";
+        $result = mysqli_query($this->connection, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        //var_dump($row);
+        if($row == NULL) {
+        
+            $sql2 = "INSERT INTO gerecht_info (id, record_type, gerecht_id, user_id, datum, nummeriekveld, tekstveld) values (NULL, 'F', $gerecht_id, $user_id, '', NULL, NULL)";
+
+            $result2 = mysqli_query($this->connection, $sql2);  
+        }      
+    }
+
+    public function deleteFavorite($gerecht_id, $user_id) {
+        $sql = "DELETE FROM gerecht_info WHERE gerecht_id = $gerecht_id and user_id = $user_id and record_type = 'F'";
+        
+        $result = mysqli_query($this->connection, $sql);
+    }
+
+}
